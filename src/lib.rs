fadf8d6fec8bdcddca1fb766501cbe11af2164d9
Michał Buczko <michal.buczko@gmail.com>
2022-12-13 01:12:19 +0100

    parsing added

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::fmt::Display;
-
 use thiserror::Error;
 
 #[derive(Debug, PartialEq)]
@@ -10,12 +9,15 @@ enum Token<'a> {
     CurlyClose,
     SquareOpen,
     SquareClose,
-    Eq,
+    Colon,
     Comma,
-    Str(&'a str),
-    Ident(&'a str),
-    Float(f64),
-    Integer(i64),
+    Equal,
+    Exclamation,
+    Contains,
+    String(&'a str),
+    Path(&'a str),
+    Float(f32),
+    Integer(i32),
     Unknown,
 }
 
@@ -46,14 +48,26 @@ pub enum TokenizeError {
     MalformedFloat(usize),
 }
 
+#[derive(Error, Debug, PartialEq)]
+pub enum ParseError {
+    #[error("malformed expression")]
+    MalformedExpression,
+    #[error("malformed filter")]
+    MalformedFilter,
+    #[error("invalid filter value")]
+    MalformedFilterValue,
+    #[error("invalid filter comparision operator")]
+    MalformedFilterOperator,
+}
+
 fn whitespace(ch: char) -> bool {
     ch == ' ' || ch == '\r' || ch == '\n' || ch == '\t'
 }
 
-fn tokenize(input: &str) -> anyhow::Result<Vec<Token>, TokenizeError> {
+fn tokenize(input: &str) -> Result<Vec<Token>, TokenizeError> {
     let mut chars = input.chars().enumerate().peekable();
     let mut output = Vec::new();
-    let mut braces = Vec::new();
+    let mut braces = Vec::with_capacity(8);
 
     while let Some((pos, ch)) = chars.next() {
         let token = match ch {
@@ -62,13 +76,13 @@ fn tokenize(input: &str) -> anyhow::Result<Vec<Token>, TokenizeError> {
                 let mut eoi = pos;
                 loop {
                     if let Some((p, ch)) = chars.peek() {
-                        if ch.is_alphanumeric() || *ch == '-' {
+                        if ch.is_alphanumeric() || *ch == '-' || *ch == '.' || *ch == '_' {
                             eoi = *p;
                             chars.next();
                             continue;
                         }
                     }
-                    break Token::Ident(&input[pos..eoi + 1]);
+                    break Token::Path(&input[pos..eoi + 1]);
                 }
             }
             b if b.is_numeric() => {
@@ -81,7 +95,7 @@ fn tokenize(input: &str) -> anyhow::Result<Vec<Token>, TokenizeError> {
                             chars.next();
                             continue;
                         }
-                        // is it a float number, maybe?
+                        // a float number, maybe?
                         if *ch == '.' {
                             if !point {
                                 chars.next();
@@ -94,7 +108,7 @@ fn tokenize(input: &str) -> anyhow::Result<Vec<Token>, TokenizeError> {
                         Token::Float(
                             input[pos..eoi + 1]
                                 .parse()
-                                .map_err(|_| TokenizeError::MalformedFloat(eoi))?
+                                .map_err(|_| TokenizeError::MalformedFloat(eoi))?,
                         )
                     } else {
                         Token::Integer(input[pos..eoi + 1].parse().unwrap())
@@ -104,7 +118,7 @@ fn tokenize(input: &str) -> anyhow::Result<Vec<Token>, TokenizeError> {
             '"' => loop {
                 if let Some((p, ch)) = chars.next() {
                     if ch == '"' {
-                        break Token::Str(&input[pos + 1..p]);
+                        break Token::String(&input[pos + 1..p]);
                     }
                 } else {
                     return Err(TokenizeError::InvalidString(pos));
@@ -149,8 +163,11 @@ fn tokenize(input: &str) -> anyhow::Result<Vec<Token>, TokenizeError> {
                     ))
                 }
             },
-            ':' => Token::Eq,
+            '!' => Token::Exclamation,
+            ':' => Token::Colon,
             ',' => Token::Comma,
+            '=' => Token::Equal,
+            '~' => Token::Contains,
             _ => Token::Unknown,
         };
         output.push(token);
@@ -158,9 +175,152 @@ fn tokenize(input: &str) -> anyhow::Result<Vec<Token>, TokenizeError> {
     Ok(output)
 }
 
+/// Parser rules:
+///
+/// expr     ->  query range?
+/// query    ->  "{" filter (COMMA filter)* "}"
+/// filter   ->  PATH op value
+/// op       ->  "!"? (EQ | CONTAINS)
+/// value    ->  STRING | BOOL | numeric
+/// numeric  ->  "-"? (INTEGER | FLOAT)
+
+enum Operator {
+    Equal,
+    Contains,
+}
+
+enum Value {
+    String(String),
+    Integer(i32),
+    Float(f32),
+    Bool(bool),
+}
+
+enum Matcher<'a> {
+    Exact(Token<'a>),
+    Path,
+    Operator,
+    Negation,
+    Value,
+    Integer,
+    Float,
+    String,
+    Numeric,
+    Bool,
+}
+struct Expr<'a> {
+    filters: Vec<Filter<'a>>,
+    range: Option<Range>,
+}
+
+struct Filter<'a> {
+    path: Vec<&'a str>, // property path split by dot
+    value: Value,
+    op: Operator,
+    negated: bool,
+}
+
+struct Range {}
+
+impl<'a> TryFrom<&Token<'a>> for Value {
+    type Error = ParseError;
+
+    fn try_from(value: &Token) -> Result<Self, Self::Error> {
+        match value {
+            Token::String(s) => Ok(Self::String(s.to_string())),
+            Token::Integer(i) => Ok(Self::Integer(*i)),
+            Token::Float(f) => Ok(Self::Float(*f)),
+            _ => Err(ParseError::MalformedFilterValue)
+        }
+    }
+}
+
+impl<'a> TryFrom<&Token<'a>> for Operator {
+    type Error = ParseError;
+
+    fn try_from(value: &Token) -> Result<Self, Self::Error> {
+        match value {
+            Token::Equal => Ok(Self::Equal),
+            Token::Contains => Ok(Self::Contains),
+            _ => Err(ParseError::MalformedFilterOperator)
+        }
+    }
+}
+
+fn match_token<'a>(
+    tokens: &'a [Token],
+    matcher: Matcher,
+) -> Option<(&'a Token<'a>, &'a [Token<'a>])> {
+    if let Some(token) = tokens.first() {
+        let result = match (matcher, token) {
+            (Matcher::Exact(t), token) if t == *token => Some(token),
+            (Matcher::Value, Token::String(_) | Token::Integer(_) | Token::Float(_)) => Some(token),
+            (Matcher::Path, Token::Path(_)) => Some(token),
+            (Matcher::Float, Token::Float(_)) => Some(token),
+            (Matcher::Integer, Token::Integer(_)) => Some(token),
+            (Matcher::Numeric, Token::Integer(_) | Token::Float(_)) => Some(token),
+            (Matcher::Operator, Token::Equal | Token::Contains) => Some(token),
+            (Matcher::Negation, Token::Exclamation) => Some(token),
+            _ => None,
+        };
+        if let Some(matched) = result {
+            return Some((matched, &tokens[1..]));
+        }
+    }
+    None
+}
+
+fn parse_expression<'a>(tokens: &'a [Token]) -> Result<Expr<'a>, ParseError> {
+    if let Some((_, tokens)) = match_token(tokens, Matcher::Exact(Token::CurlyOpen)) {
+        let (filters, _) = parse_filters(tokens)?;
+        let range = Some(Range {});
+
+        return Ok(Expr { filters, range });
+    }
+    Err(ParseError::MalformedExpression)
+}
+
+fn parse_filters<'a>(
+    tokens: &'a [Token],
+) -> Result<(Vec<Filter<'a>>, &'a [Token<'a>]), ParseError> {
+    let mut filters = Vec::<Filter>::new();
+
+    // look for property path
+    if let Some((Token::Path(id), tokens)) = match_token(tokens, Matcher::Path) {
+        // look for operator (equal, contains, ...) and potential negation
+        let negated = match_token(tokens, Matcher::Negation).is_some();
+        if let Some((op, tokens)) = match_token(tokens, Matcher::Operator) {
+            // look for a value and compose a `Filter` if all elements have been matched correctly
+            if let Some((val, _)) = match_token(tokens, Matcher::Value) {
+                filters.push(Filter {
+                    path: id.to_owned().split('.').collect::<Vec<_>>(),
+                    value: Value::try_from(val)?,
+                    op: Operator::try_from(op)?,
+                    negated
+                });
+            }
+        }
+    }
+    Err(ParseError::MalformedFilter)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn token_matching() {
+        let tokens = tokenize("{ meta.focal_length }").unwrap();
+
+        let tokens = match_token(tokens.as_slice(), Matcher::Exact(Token::CurlyOpen));
+        assert!(tokens.is_some());
+
+        let tokens = match_token(tokens.unwrap().1, Matcher::Path);
+        assert_eq!(tokens.unwrap().0, &Token::Path("meta.focal_length"));
+
+        let tokens = match_token(tokens.unwrap().1, Matcher::Exact(Token::CurlyClose));
+        assert!(tokens.is_some());
+    }
 
     #[test]
     fn simple_tokens() {
@@ -182,7 +342,7 @@ mod tests {
             result,
             vec![
                 Token::CurlyOpen,
-                Token::Str("lorem ipsum"),
+                Token::String("lorem ipsum"),
                 Token::CurlyClose
             ]
         );
@@ -204,15 +364,15 @@ mod tests {
         assert_eq!(TokenizeError::UnbalancedBraces(4, Brace::Curly), result);
     }
     #[test]
-    fn ident() {
+    fn path() {
         let result = tokenize("{mime-type:\"image/png\"}").unwrap();
         assert_eq!(
             result,
             vec![
                 Token::CurlyOpen,
-                Token::Ident("mime-type"),
-                Token::Eq,
-                Token::Str("image/png"),
+                Token::Path("mime-type"),
+                Token::Colon,
+                Token::String("image/png"),
                 Token::CurlyClose
             ]
         );
@@ -224,8 +384,8 @@ mod tests {
             result,
             vec![
                 Token::CurlyOpen,
-                Token::Ident("focal-length"),
-                Token::Eq,
+                Token::Path("focal-length"),
+                Token::Colon,
                 Token::Integer(32),
                 Token::CurlyClose
             ]
@@ -238,8 +398,8 @@ mod tests {
             result,
             vec![
                 Token::CurlyOpen,
-                Token::Ident("width"),
-                Token::Eq,
+                Token::Path("width"),
+                Token::Colon,
                 Token::Float(32.122),
                 Token::CurlyClose
             ]
@@ -249,25 +409,25 @@ mod tests {
             result,
             vec![
                 Token::CurlyOpen,
-                Token::Ident("width"),
-                Token::Eq,
+                Token::Path("width"),
+                Token::Colon,
                 Token::Float(32.122),
                 Token::Unknown,
                 Token::Integer(3),
                 Token::CurlyClose
             ]
         );
-        let result = tokenize("{width: 32.}").unwrap();
+        let result = tokenize("{width: 32..}").unwrap();
         assert_eq!(
             result,
             vec![
                 Token::CurlyOpen,
-                Token::Ident("width"),
-                Token::Eq,
+                Token::Path("width"),
+                Token::Colon,
                 Token::Float(32.0),
+                Token::Unknown,
                 Token::CurlyClose
             ]
         );
-
     }
 }
